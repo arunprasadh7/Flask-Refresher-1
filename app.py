0e670087b5a8745a4cff213fc059046acc9007d3
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-01-18 21:31:57 +0530

    Routing in flask

--- app.py
+++ app.py
@@ -2,9 +2,27 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+# Routing in flask 
+
+
+# 1 - Routing using decorators 
+
 @app.route('/')
-def home():
-    return 'Welcome to home Page'
+def login_page():
+    return 'Welcome to login page!!'
+
+@app.route('/home')
+def home_page():
+    return 'Welcome to home page.'
+
+
+
+# 2 - Routing without using decorators
+
+# app.add_url_rule('/','login',login_page)
+# app.add_url_rule('/home_page','home_page',home_page)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
